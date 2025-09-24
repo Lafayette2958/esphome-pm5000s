75a0a49cd4e656dad3b4ea99882b66dc9df3e668
Lafayette2958 <Institutlafayette2958@gmail.com>
2025-09-24 15:27:19 +0200

    Update pm5000s.h

--- components/pm5000s/pm5000s.h
+++ components/pm5000s/pm5000s.h
@@ -1,26 +1,35 @@
-#ifndef PM5000S_H
-#define PM5000S_H
+#pragma once
 
-#include "esphome.h"
+#include "esphome/core/component.h"
+#include "esphome/components/i2c/i2c.h"
+#include "esphome/components/sensor/sensor.h"
+
+namespace esphome {
+namespace pm5000s {
 
 class PM5000SSensor : public PollingComponent, public i2c::I2CDevice {
  public:
-  Sensor *particles_03um = new Sensor();
-  Sensor *particles_05um = new Sensor();
-  Sensor *particles_10um = new Sensor();
-  Sensor *particles_25um = new Sensor();
-  Sensor *particles_50um = new Sensor();
-  Sensor *particles_100um = new Sensor();
+  // Sensor pointers, not new instances
+  sensor::Sensor *particles_03um{nullptr};
+  sensor::Sensor *particles_05um{nullptr};
+  sensor::Sensor *particles_10um{nullptr};
+  sensor::Sensor *particles_25um{nullptr};
+  sensor::Sensor *particles_50um{nullptr};
+  sensor::Sensor *particles_100um{nullptr};
 
-  PM5000SSensor(uint8_t address = 0x28, i2c::I2CBus *bus = nullptr) : PollingComponent(5000) {
-    this->set_i2c_address(address);
-    if (bus != nullptr) {
-      this->set_i2c_bus(bus);
-    }
-  }
+  PM5000SSensor(uint8_t address = 0x28) : PollingComponent(10000), i2c::I2CDevice(address) {}
 
   void setup() override;
   void update() override;
+
+  // Setters for each particle size
+  void set_particles_03um_sensor(sensor::Sensor *sensor) { this->particles_03um = sensor; }
+  void set_particles_05um_sensor(sensor::Sensor *sensor) { this->particles_05um = sensor; }
+  void set_particles_10um_sensor(sensor::Sensor *sensor) { this->particles_10um = sensor; }
+  void set_particles_25um_sensor(sensor::Sensor *sensor) { this->particles_25um = sensor; }
+  void set_particles_50um_sensor(sensor::Sensor *sensor) { this->particles_50um = sensor; }
+  void set_particles_100um_sensor(sensor::Sensor *sensor) { this->particles_100um = sensor; }
 };
 
-#endif  // PM5000S_H
+}  // namespace pm5000s
+}  // namespace esphome
